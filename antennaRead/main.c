4400397f1d45f198a380eded7f11b2cb131fe6ba
Sean <sheath51@gmail.com>
2019-02-05 08:10:54 -0700

    Added gitignore and other content

--- antennaRead/main.c
+++ antennaRead/main.c
@@ -2,38 +2,42 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#include "mailbox.h"
-#include "gpu_fft.h"
 #include "computefft.h"
 
 int main(){
 	int numData = 8000;
-	int zeroPad = 8192;
-	double sigData[zeroPad];
-	FILE *myFile;
-	myFile = fopen("testSignal.txt","r");
-	if(myFile == NULL){
+	int zeroPad = 8192*2;
+	double data[zeroPad];
+	FILE *dataIn;
+    FILE *dataOut;
+
+	dataIn = fopen("testSignal.txt","r");
+	if(dataIn == NULL){
 		printf("Cannot open file\n\r");
 		return -1;
 	}
 
-	int i;
+	int i,j;
 	double buffer;
 	for(i=0; i<numData; i++){
-		fscanf(myFile, "%lf", &buffer);
-		sigData[i] = buffer;
-		//printf("%.15f ", sigData[i]);
+		fscanf(dataIn, "%lf", &buffer);
+		data[i] = buffer;
 	}
+    for(j=i; j<zeroPad; j++) data[j] = 0;
 
-    /*
-	for(int j=numData; j<zeroPad; j++){
-		sigData[j] = 0;
-		printf("%.15f ", sigData[j]);
-	}
-    */
-
-	double* data = sigData;
+    fclose(dataIn);
 	computefft(data, 13);
+
+    dataOut = fopen("signalOut.txt","w");
+    if(dataOut == NULL){
+        printf("Cannot create file\n\r");
+        return -2;
+    }
+
+    for(i=0; i<zeroPad; i++){
+        //printf("loop #: %d, real value: %0.3lf\n", i, data[i]);
+        fprintf(dataOut, "%0.3lf %0.3lf\n", data[i], data[i++]);
+    }
 
 	return 0;
 }
