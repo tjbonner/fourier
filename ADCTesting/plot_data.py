a4c007f5dec5c95718c86fbf9433e2c0f5ab844b
Jacob Holtom <jacob@holtom.me>
2019-02-22 17:00:15 -0700

    Added ADCTesting

---
+++ ADCTesting/plot_data.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.interpolate import griddata
+import math
+from scipy.signal import decimate, convolve
+RawData ="30khz.bin"
+f = open(RawData, "r")
+a = np.fromfile(f, dtype=np.uint32)
+t = 200000000
+V = a[:-1].copy()
+T = [ (( x * t ) / ( 1000.0*len(V) )) for x in 2*range(len(V))]
+
+ADC1_GPIO = [9,25,10,22,27,17,18,15,14,24]
+
+ADC2_GPIO = [20,26,16,19,13,12,7,8,11,21]
+
+ADC1len = len(ADC1_GPIO)
+ADC2len = len(ADC2_GPIO)
+
+def GetV2(Volts):
+    Signal = []
+    Map = np.zeros((len(V),ADC2len), dtype=np.int)
+    for i in range(len(Volts)):
+        val = int(V[i])
+        SignalZero = 0
+        for k in range(ADC2len):
+            Map[i][k] = (val & 2**k)/2**k
+        for k in range(ADC2len):
+            SignalZero += 2**k*((val & 2**ADC2_GPIO[k])/2**ADC2_GPIO[k])
+        Signal.append(SignalZero)
+
+
+    return Signal,Map
+
+def GetV1(Volts):
+    Signal = []
+    Map = np.zeros((len(V),ADC1len), dtype=np.int)
+    for i in range(len(Volts)):
+        val = int(V[i])
+        SignalZero = 0
+        for k in range(ADC1len):
+            Map[i][k] = (val & 2**k)/2**k
+        for k in range(ADC1len):
+            SignalZero += 2**k*((val & 2**ADC1_GPIO[k])/2**ADC1_GPIO[k])
+        Signal.append(SignalZero)
+
+
+    return Signal,Map
+
+M = GetV2(V)[0]
+M2 = GetV1(V)[0]
+Ma = M - np.average(M[5000:7000])
+Mb = M2 - np.average(M2[5000:7000])
+
+plt.figure(figsize=(15,5))
+plt.plot(Mb,"b")
+plt.title("Plots "+RawData+' .')
+plt.show()
